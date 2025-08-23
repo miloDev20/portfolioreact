4c6509262616556367f674cd3b56689a91853d0f
al405059 <emiliobm45@gmail.com>
2025-08-23 13:13:50 +0200

    Make background fixed

--- portfolio/src/Components/AnimatedStars.jsx
+++ portfolio/src/Components/AnimatedStars.jsx
@@ -4,7 +4,7 @@ import Star from "./Star";
 export default function AnimatedStars() {
   // Generamos un array de estrellas
   return (
-    <div className="absolute inset-0 -z-10 overflow-hidden">
+    <div className="fixed inset-0 -z-10 overflow-hidden">
       {Array.from({ length: 20 }).map((_, i) => (
         <Star key={i} size={20 + Math.random() * 10} />
       ))}
