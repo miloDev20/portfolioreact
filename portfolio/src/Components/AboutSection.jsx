5ff2ec09638a7032b81f633ee9f1d05b3b955f1a
Emilio <emiliobm45@gmail.com>
2025-09-07 10:15:53 +0200

    upload cv pdf

--- portfolio/src/Components/AboutSection.jsx
+++ portfolio/src/Components/AboutSection.jsx
@@ -50,7 +50,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/cvDeveloper.pdf"
+                href="../src/assets/cvDeveloper.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
